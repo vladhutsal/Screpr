1661176a48e161f0280a0ad5b10312cf99d00ce3
vlad <vl.hutsal@gmail.com>
2020-09-14 14:23:26 +0300

    add automatic folder creation feature add json_validation to check if config.json is having right structure add exception_handling func repair do_the_move func rename maintance func to arg_parsing

--- screpr_new.py
+++ screpr_new.py
@@ -4,45 +4,65 @@ import os
 import json
 import argparse
 import jsonschema
+from pprint import pprint
+
+
+def json_validation(config_dict):
+    validator = {
+    "patternProperties": {
+        ".+": {"type": "array"}
+    }
+}
+    return jsonschema.validate('config_dict', validator)
 
 
 def load_config(config_path) -> dict():
     with open(config_path, 'r') as read_file:
         config_dict = json.load(read_file)
+        validate_fail = json_validation(config_dict)
+    if not validate_fail:
+        format_to_path = dict()
+        check_folders(config_dict)
+        for path, formats in config_dict.items():
+            for frmt in formats:
+                format_to_path[frmt] = path
+        return format_to_path
+    else:
+        exception_handling(validate_fail)
 
-    format_to_path = dict()
-    for path, formats in config_dict.items():
-        for frmt in formats:
-            format_to_path[frmt] = path
-    return format_to_path
+
+def check_folders(config_dict):
+    for new_folder_path in config_dict.keys():
+        if os.path.isdir(new_folder_path) == False:
+             os.mkdir(new_folder_path)
 
 
-def walk(config, sort_path):
-    print('replacing files..')
+def walk(config_dict, sort_path):
     for path, _, files in os.walk(sort_path):
         for filename in files:
             file_format = filename.split('.')[-1]
-            dest = need_to_move(config, file_format)
+            dest = need_to_move(config_dict, file_format)
             if dest:
                 src = f'{path}/{filename}'
-                do_the_move(config, src, f'{dest}/{filename})
+                do_the_move(src, f'{dest}/{filename}')
 
 
-def need_to_move(config, file_format):
-    if file_format in config.keys():
-        return config[file_format]
+def need_to_move(config_dict, file_format):
+    if file_format in config_dict.keys():
+        return config_dict[file_format]
     return None
 
 
-def do_the_move(config, src, dst):
-    pass
+def do_the_move(src, dst):
+    print(f'{src} => {dst}')
+    # os.replace(src, dst)
 
 
-def maintance():
-    def_config_name = '/screpr_config.json'
+def arg_parsing():
+    default_config_name = '/screpr_config.json'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', default=os.getcwd() + def_config_name,
+    parser.add_argument('-c', default=os.getcwd() + default_config_name,
                 help='Config path(default: current dir)',
                 metavar='/home/../cfg.json',
                 type=str,
@@ -56,15 +76,18 @@ def maintance():
     return parser.parse_args().config, parser.parse_args().sp
 
 
+def exception_handling(excpt):
+    if excpt:
+        return f'Something went wrong: {str(excpt)}'
+
+
 def main():
-    config_path, sort_path = maintance()
+    config_path, sort_path = arg_parsing()
     try:
-        config = load_config(config_path)
-        print('Config is loaded..')
-        walk(config, sort_path)
-        print('done!')
-    except Exception as e:
-        print(f'Wrong config file: {str(e)}')
+        config_dict = load_config(config_path)
+        walk(config_dict, sort_path)
+    except Exception as excpt:
+        print(exception_handling(excpt))
 
 
 if __name__ == "__main__":
