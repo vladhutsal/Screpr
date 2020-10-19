31a897441ddcc2296128a7b8680252629b12fbc6
vlad <vl.hutsal@gmail.com>
2020-10-19 17:22:41 +0300

    combined base_screpr and Screpr.py - now base_screpr works with Screpr class

--- base_screpr.py
+++ base_screpr.py
@@ -2,76 +2,81 @@
 
 import re
 import os
+import shutil
 import json
 import argparse
 import jsonschema
 
 
-def conf_to_dict(config) -> dict():
-    config_dict = dict()
-    config.pop('mode')
-    for path, values in config.items():
-        for value in values:
-            config_dict[value] = path
-    return config_dict
+class Screpr:
+    def __init__(self, workdir_path, cfg_path, mode, log):
+        self.work_dir_path = workdir_path
+        self.cfg_path = cfg_path
+        self.mode = mode
+        self.log = log
 
+        self.user_cfg = self.load_cfg()
+        self.screpr_cfg = self.user_cfg_to_dict()
 
-def load_config(config_path):
-    with open(config_path, 'r') as read_file:
-        config = json.load(read_file)
-        validation_fail = json_validation(config)
-        if validation_fail:
-            raise Exception
-        else:
+    def load_cfg(self):
+        with open(self.cfg_path, 'r') as read_file:
+            config = json.load(read_file)
+            validation_fail = self.json_validation(config)
+            if validation_fail:
+                raise Exception
+
             return config
 
+    def user_cfg_to_dict(self):
+        screpr_cfg = {}
+        for path, values in self.user_cfg.items():
+            for value in values:
+                screpr_cfg[value] = path
 
-def json_validation(config_dict):
-    validator = {
-        "patternProperties": {
-            ".+": {"type": "array"}
+        return screpr_cfg
+
+    def json_validation(self, config):
+        validator = {
+            "patternProperties": {
+                ".+": {"type": "array"}
+            }
         }
-    }
-    return jsonschema.validate(config_dict, validator)
+        return jsonschema.validate(config, validator)
 
 
-def create_folders(config):
+def check_folders(config):
     for new_folder_path in config.keys():
         if not os.path.isdir(new_folder_path):
             os.mkdir(new_folder_path)
 
 
-def walk_trhough_files(working_dir, config_dict, mode):
-    for path, _, files in os.walk(working_dir):
+def walk_trhough_files(screpr):
+    for folder_path, _, files in os.walk(screpr.work_dir_path):
         if not files:
             raise Exception('Work folder is empty')
+
         for filename in files:
-            if mode == 'regex':
-                dest = need_to_move_regex(filename, config_dict)
-            elif mode == 'format-sort':
-                file_format = filename.split('.')[-1]
-                dest = need_to_move_format(config_dict, file_format)
-            else:
-                raise Exception('Wrong config mode')
+            dest = need_to_move(filename, screpr.screpr_cfg)
             if dest:
-                do_the_move(path, dest, filename)
+                do_the_job(screpr.mode, folder_path, dest, filename)
 
 
-def need_to_move_regex(filename, config_dict):
-    for key in config_dict.keys():
+def need_to_move(filename, screpr_cfg):
+    for key in screpr_cfg.keys():
         match = re.search(key, filename)
         if match:
-            return config_dict[key]
+            return screpr_cfg[key]
 
 
-def need_to_move_format(config_dict, file_format):
-    if file_format in config_dict.keys():
-        return config_dict[file_format]
-    return None
+# function to copy or move files
+def do_the_job(mode, folder_path, dest, filename):
+    src = f'{folder_path}/{filename}'
+    dst = f'{dest}/{filename}'
 
-
-def do_the_move(path, dest, filename):
-    os.replace(f'{path}/{filename}', f'{dest}/{filename}')
+    if mode == 'safe':
+        shutil.copyfile(src, dst)
+    elif not mode or mode == 'move':
+        shutil.move(src, dst)
 
 
 def arg_parsing():
@@ -94,25 +99,10 @@ def arg_parsing():
     return parser.parse_args().path, parser.parse_args().config
 
 
-def screpr(working_dir, config):
-    mode = config.get('mode')[0]
-    config_dict = conf_to_dict(config)
-    create_folders(config)
-    walk_trhough_files(working_dir, config_dict, mode)
-    print('all done')
-
-
-def main():
-    try:
-        working_dir, config_path = arg_parsing()
-        config = load_config(config_path)
-        screpr(working_dir, config)
-    except Exception as excpt:
-        print(f'Something went wrong: {excpt}')
-
-
-if __name__ == "__main__":
-    main()
-
-
-
+def screpr(working_dir, config_path, *args, **kwargs):
+    mode = kwargs.get('mode') or None
+    log = kwargs.get('log') or False
+    screpr = Screpr(working_dir, config_path, mode, log)
+    check_folders(screpr.user_cfg)
+    walk_trhough_files(screpr)
+    return 'done?'
--- work_with_screpr.py
+++ work_with_screpr.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
 
-import Screpr
+import base_screpr
 import os
 import tempfile
-import re
+
+debug = False
+create_files = False
 
 
-
-# debug = False
-# create_files = False
-
-# def generate_tmpfiles():
-#     for frmt in range(3):
-#         tempfile.NamedTemporaryFile(suffix='',
-#                                     dir=path,
-#                                     delete=False)
+def generate_tmpfiles():
+    files = ['jpg', 'jpeg', 'avi', 'mp3', 'doc', 'txt']
+    for frmt in files:
+        tempfile.NamedTemporaryFile(suffix='',
+                                    dir=path,
+                                    delete=False)
 
 
-# def debuger(*args):
-#     if create_files:
-#         generate_tmpfiles()
+def debuger(*args):
+    if create_files:
+        generate_tmpfiles()
 
-#     if debug:
-#         print('CONFIG FILE === ', cfg)
-#         print('CONFIG DICT === ', cfg_dict)
+    if debug:
+        print('CONFIG FILE === ', cfg)
+        print('CONFIG DICT === ', cfg_dict)
 
 
 if __name__ == "__main__":
     path = os.path.join(os.getcwd() + '/SORTFOLDER')
     config_path = ('screpr_config.json')
-    result = Screpr.safe(path, config_path)
+    result = base_screpr.screpr(path, config_path, mode='safe')
     print(result)
